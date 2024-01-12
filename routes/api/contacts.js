223ae782b0a5d19a07ce1cea5a2541bf91bee5f9
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2024-01-12 20:37:23 +0100

    update

---
+++ routes/api/contacts.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody, checkBody } = require("../../middlewares");
+
+const schemas = require("../../schemas/contacts");
+
+const router = express.Router();
+
+router.get("/", ctrl.listContacts);
+
+router.get("/:id", ctrl.getContactById);
+
+router.post("/", checkBody, validateBody(schemas.addSchema), ctrl.addContact);
+
+router.delete("/:id", ctrl.removeContact);
+
+router.put(
+  "/:id",
+  checkBody,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
+
+module.exports = router;
